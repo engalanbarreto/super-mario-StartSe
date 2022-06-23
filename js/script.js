df68cca699d276b34d1c778598f323b8e52693c1
Alan Barreto <eng.alanbarreto@gmail.com>
2022-06-23 13:43:45 -0300

    adiciona arquivos

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector('.super-mario');
+const pipe = document.querySelector('.pipe-game');
+
+// Arrow fuction, faz o mesmo efeito da function, porem com escrita atualizada
+// Nesse caso, foi criada a função 'jump' que pega a const maria e adiciona mais
+// uma classe chamada 'jump-mario'
+const jump = () => {
+    mario.classList.add('jump-mario');
+    //a função 'setTimeout' é uma função usada no JS
+    // usar uma função em determinado tempo,
+    // para isso é passado 2 parametros. A função e o tempo
+    setTimeout(() => {
+        mario.classList.remove('jump-mario');
+    }, 500);
+}
+
+//a função 'setInterval' é uma função usada no JS
+// usar uma função em determinado intervalo de tempo,
+// para isso é passado 2 parametros. A função e o tempo   
+const loopGame = setInterval(() => {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './images/mario-game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '45px';
+
+    clearInterval(loopGame);
+})
+
+document.addEventListener('keydown', jump);
